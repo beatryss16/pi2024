052ff542091c3a4515100125441dbe9b175d8f61
Anna Beatrys da Silva Souza <a.beatrys@academico.ifrn.edu.br>
2024-10-25 11:38:03 -0300

    Configurando dependencias do banco de dados

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,13 +1,26 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifrn.pi.eventos.models.Evento;
 
 @Controller
 public class EventosController {
-
+	
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
 	}
+
+	@PostMapping ("eventos/submit")
+	public String submitForm(Evento evento){
+		System.out.println("Nome:" + evento.getNome());
+		System.out.println("Local:" + evento.getLocal());
+		System.out.println("Data:" + evento.getData());
+		System.out.println("Horário:" + evento.getHorario());
+		return "submit";
+	}
 }
+
